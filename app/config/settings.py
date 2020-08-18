d5cf7f98817ad1a80f9fc5891897510402bb0210
kangmina92 <kmn@geoize.net>
2020-08-18 17:30:21 +0900

    first commit

--- app/config/settings.py
+++ app/config/settings.py
@@ -29,7 +29,6 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 # Application definition
-
 INSTALLED_APPS = [
     'members',
     'django.contrib.admin',
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'django_extensions',
 ]
 
@@ -125,8 +125,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # s3
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID =
-AWS_SECRET_ACCESS_KEY = 
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
 AWS_STORAGE_BUCKET_NAME = 'pbs-13-s3'
 AWS_AUTO_CREATE_BUCKET = True
 AWS_S3_REGION_NAME = 'ap-northeast-2'
