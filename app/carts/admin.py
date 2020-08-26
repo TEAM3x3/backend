857e9aab14010959ca9843ea26c8ef9fc12ca224
sanghee1115 <sanghee.kim1115@gmail.com>
2020-08-26 15:44:44 +0900

    cart 구현초기

--- app/carts/admin.py
+++ app/carts/admin.py
@@ -3,11 +3,11 @@ from carts.models import Cart, CartItem
 
 
 class CartsAdmin(admin.ModelAdmin):
-    list_display = ['cart_id', 'created_at']
+    list_display = ['cart_id', 'created_at',]
 
 
 class CartItemAdmin(admin.ModelAdmin):
-    list_display = ['user', 'goods', 'quantity']
+    list_display = ['user', 'goods', 'quantity',]
 
 admin.site.register(Cart, CartsAdmin)
 admin.site.register(CartItem, CartItemAdmin)
