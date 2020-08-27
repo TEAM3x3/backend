b78b9aad9b61ffdcf4dabe0f97c735ecf4f0d470
kangmina92 <kmn@geoize.net>
2020-08-27 17:11:28 +0900

    feature: 장바구니 기능 구현2

--- app/carts/tests.py
+++ app/carts/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from goods.models import Goods
+
+User = get_user_model()
+
+
+class Cart_test(APITestCase):
+
+    def setUp(self) -> None:
+        self.user = User(username='test', password='1111', email='cccc@c.com')
+        self.user.set_password(self.user.password)
+        self.user.save()
+
+        self.goods = Goods.objects.all()[:3]
+
+        print('dddd', self.goods)
+        self.fail()
+
+
+    def test_list(self):
+        self.fail()
