15c7d66fdbf521a41f27b59085cd6d0d57623f8f
Jeon Young Hoon <hungyb0924@gmail.com>
2020-09-14 16:40:57 +0900

    update

--- app/carts/models.py
+++ app/carts/models.py
@@ -53,6 +53,7 @@ class CartItem(models.Model):
         self.cart.quantity_of_goods = F('quantity_of_goods') + 1
         self.cart.save()
 
+    @transaction.atomic
     def delete(self, using=None, keep_parents=False):
         super().save()
         self.cart.quantity_of_goods = F('quantity_of_goods') - 1
